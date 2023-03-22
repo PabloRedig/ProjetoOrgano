0e0ad6b54f75b7760062d7a453b1c14bea20eb8b
pablo Redig <pabloredigmrv@gmail.com>
2023-03-22 16:36:25 -0300

    Criado o Campo de time, e criado o botao

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,7 +1,19 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
 const Formulario = () =>{
+    
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'Ux e Design',
+        'Mobile',
+        'Inovação e Gestao'
+    ]
     return(
         <section className="formulario">
             <form> 
@@ -9,7 +21,11 @@ const Formulario = () =>{
                 <CampoTexto label="Nome" placeholder="Digite seu Nome" />
                 <CampoTexto label="Cargo" placeholder="Digite seu Cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da Imagem" /> 
-            </form>
+                <ListaSuspensa label="Time" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form> 
         </section>
     )
 }   
